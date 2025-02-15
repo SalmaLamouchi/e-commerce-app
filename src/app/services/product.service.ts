c4fecdcafa5e9a18389e175fdc4596aa80d0fa91
salma lamouchi <SalmaLamouchi>
2025-02-15 18:59:30 +0100

    service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Product } from '../models/product'; // Assurez-vous que le chemin est correct
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Product } from '../models/product'; // Assurez-vous que le chemin est correct
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ProductService {
-  private apiUrl = 'http://localhost:3000/api/products'; // Remplacez par l'URL de votre API
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProductService {
+//   private apiUrl = 'http://localhost:3000/api/products'; // Remplacez par l'URL de votre API
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
-  // Fonction pour récupérer tous les produits
-  getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
-  }
-}
+//   // Fonction pour récupérer tous les produits
+//   getAllProducts(): Observable<Product[]> {
+//     return this.http.get<Product[]>(this.apiUrl);
+//   }
+// }
